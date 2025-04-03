1be0216530919e045ed1eb201985f3bd4e0a1331
nAIDIK <you@example.com>
2025-04-03 16:35:39 +0530

    react

---
+++ my-first-app/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Title from "./title.jsx";
+import Card from "./card.jsx"
+
+function App() {
+
+  return (
+    <>
+    <Card />
+    </>
+  )
+}
+
+export default App
---
+++ my-first-app/src/card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./card.css"
+
+const card = () => {
+  return (
+    <div>
+        <div className='card'>
+        <h1>Card</h1>
+        </div>
+        <card />        
+    </div>
+    
+  )
+}
+
+export default card
