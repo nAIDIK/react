ab58316e45fa7226750ffcc99ed8c0599f86f895
nAIDIK <you@example.com>
2025-04-07 16:00:08 +0530

    React

--- my-first-app/src/App.jsx
+++ my-first-app/src/App.jsx
@@ -1,15 +1,57 @@
 
-import React from 'react'
-import Card from "./card.jsx"
+// 05-04-25
 
-const App = () => {
-  return (
+//  01 =>how to use button in react
+//  lets see =>
 
-    <div>
-   <Card />
-    </div>
+// import React from 'react'
+// import Card from "./card.jsx"
+
+// const App = () => {
+//   return (
+
+//     <div>
+//    {/* <Card /> */}
+//     </div>
     
-  )
-}
+//   )
+// }
 
-export default App;
+// export default App;
+
+// 07-04-25
+
+ //  02 => how to use different buttons  in same react projects 
+ // lets see =>
+
+// import React from 'react'
+// import Button from "./button.jsx"
+
+// const App = () => {
+//   return (
+//     <div>
+//        <Button name="click me"/>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// 03 => create a profil of your self and pass a propes of your name and age 
+// lets see =>
+
+  // import React from 'react'
+  // import Profile from "./Profile.jsx"
+
+  // const App = () => {
+  //   return (
+  //     <div>
+  //     <Profile name="Vaidik" age="20"/>
+  //       </div>
+  //   )
+  // }
+  
+  // export default App
+
+
+  
---
+++ my-first-app/src/Profile.jsx
@@ -0,0 +1,10 @@
+
+const Profile = (props) => {
+  return (
+    <div>
+        <h1>my name is {props.name} and my age is {props.age}</h1>
+    </div>
+  )
+}
+
+export default Profile
