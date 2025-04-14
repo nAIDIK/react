b4e68e232f8567b334816bd79c8eb90e5cc3e878
nAIDIK <you@example.com>
2025-04-14 16:46:04 +0530

    React Routin

--- my-second-app/src/App.jsx
+++ my-second-app/src/App.jsx
@@ -7,6 +7,10 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
+import Blogs from "./Components/Blogs";
+import Contact from "./Components/Contact";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
 
 const App = () => {
   return(
@@ -14,17 +18,25 @@ const App = () => {
     <Router>
 
     <Link to="/">Home</Link>
-    &nbsp;   &nbsp;
+    &nbsp;   &nbsp;  &nbsp;
     <Link to="/about">About</Link>
+    &nbsp;   &nbsp;  &nbsp;
+    <Link to="/blogs">Blogs</Link>
+    &nbsp;   &nbsp;  &nbsp;
+    <Link to="/contact">Contact</Link>
+    &nbsp;   &nbsp;  &nbsp;
+    <Link to="/login">Login</Link>
+    &nbsp;   &nbsp;  &nbsp;
+    <Link to="/signup">Signup</Link>
 
       <Routes>
 
         <Route path="/" element={<Home />}></Route>
         <Route path="/about" element={<About />}></Route>
-        <Route></Route>
-        <Route></Route>
-        <Route></Route>
-        <Route></Route>
+        <Route path="/blogs" element={<Blogs />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
 
       </Routes>
 
